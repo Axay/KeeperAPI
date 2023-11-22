12b9a021a8fea7e938b332b03e4148814f8fb4e5
Akshay Shailesh <akshayshailesh@gmail.com>
2023-11-22 19:32:23 +0530

    Mongo Password Updated

--- server.js
+++ server.js
@@ -8,48 +8,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const pass = encodeURIComponent(process.env.PASS);
-const user = process.env.USER;
+const pass = xWtSEihHaXxqvmQv;
+const user = akshayshailesh;
 
 var db1 = mongoose.connect(
-  "mongodb+srv://" +
-    user +
-    ":" +
-    pass +
-    "@cluster0.v0qezj7.mongodb.net/keeperDB?retryWrites=true&w=majority"
+	"mongodb+srv://" +
+		user +
+		":" +
+		pass +
+		"@cluster0.v0qezj7.mongodb.net/keeperDB?retryWrites=true&w=majority"
 );
 var db = mongoose.connection;
 
 const keeperSchema = {
-  title: String,
-  content: String,
+	title: String,
+	content: String,
 };
 
 const Keeper = mongoose.model("Keeper", keeperSchema);
 
 app.get("/api/getAll", (req, res) => {
-  Keeper.find({}).then(function (KeeperList) {
-    res.send(KeeperList);
-  });
+	Keeper.find({}).then(function (KeeperList) {
+		res.send(KeeperList);
+	});
 });
 
 app.post("/api/addNew", (req, res) => {
-  const { title, content } = req.body;
-  const keeperObj = new Keeper({
-    title: title,
-    content: content,
-  });
-  keeperObj.save();
-  res.redirect("/api/getAll");
+	const { title, content } = req.body;
+	const keeperObj = new Keeper({
+		title: title,
+		content: content,
+	});
+	keeperObj.save();
+	res.redirect("/api/getAll");
 });
 
 app.post("/api/delete", (req, res) => {
-  const { id } = req.body;
-  Keeper.deleteOne({ _id: id }).then(function () {
-    res.redirect("/api/getAll");
-  });
+	const { id } = req.body;
+	Keeper.deleteOne({ _id: id }).then(function () {
+		res.redirect("/api/getAll");
+	});
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("backend created at port 5000");
+	console.log("backend created at port 5000");
 });
